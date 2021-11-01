592199843b60153e8175c84b8ac8b7d775875f60
shivakar <shivakar@ch.iitr.ac.in>
2021-11-02 00:10:11 +0530

    [shivakar] ADD :add find maximum

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindMaxUsingGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*  UC1_Find_Max check = new UC1_Find_Max();
+              check.checkMax(3, 5, 5);*/
+
+            /*  UC2_FindMaxFloat  check = new UC2_FindMaxFloat();
+              check.FindMax(1.5, 5.1565, 5.894);*/
+            UC3_FindMaxString check = new UC3_FindMaxString();
+            check.FindMax("44","260","56");
+
+
+
+        }
+    }
+}
---
+++ UC1_Find_Max.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxUsingGeneric
+{
+    class UC1_Find_Max
+    {
+        
+        public void checkMax(int a, int  b, int c)
+        {
+            if ((a.CompareTo(b)>0) && (a.CompareTo(c)>0))
+            {
+                Console.WriteLine(a + " is maximum");
+
+            }
+            if ((b.CompareTo(c) > 0) && (b.CompareTo(a) > 0))
+            {
+                Console.WriteLine(b + " is maximum");
+
+            }
+            if ((c.CompareTo(a) > 0) && (c.CompareTo(b) > 0))
+            {
+                Console.WriteLine(c + " is maximum");
+            }
+            else
+                Console.WriteLine("may be some have same value plz check input again.");
+
+        }
+    }
+}
