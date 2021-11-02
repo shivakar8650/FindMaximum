2d705b4ae764c9e4a5126d3a59f9d11d1eb31636
shivakar <shivakar@ch.iitr.ac.in>
2021-11-02 22:40:16 +0530

    [shivakar] ADD:find max using generic added

---
+++ GenricMaximum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxUsingGeneric
+{
+    class GenricMaximum<T> where T : IComparable
+    {
+        public T a, b, c;
+
+        public GenricMaximum(T a, T b, T c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public static T FindMax(T a, T b,T c)
+        {
+
+            if ((a.CompareTo(b) > 0) && (a.CompareTo(c) > 0) ||
+                (a.CompareTo(b) >= 0) && (a.CompareTo(c) > 0) ||
+                (a.CompareTo(b) > 0) && (a.CompareTo(c) >= 0))
+            {
+                return a;
+
+            }
+            if ((b.CompareTo(c) > 0) && (b.CompareTo(a) > 0) ||
+                (b.CompareTo(c) >= 0) && (b.CompareTo(a) > 0) ||
+                (b.CompareTo(c) > 0) && (b.CompareTo(a) >= 0))
+            {
+                return b;
+
+            }
+            if ((c.CompareTo(a) > 0) && (c.CompareTo(b) > 0) ||
+                (c.CompareTo(a) >= 0) && (c.CompareTo(b) > 0) ||
+                (c.CompareTo(a) > 0) && (c.CompareTo(b) >= 0))
+            {
+                return c;
+            }
+            return default;
+        }
+
+        public T testMaximum()
+        {
+            T maximum = GenricMaximum<T>.FindMax( this.a, this.b, this.c);
+            return maximum;
+        }
+    }
+}
---
+++ UC5_PrintMax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxUsingGeneric
+{
+    class UC5_PrintMax<T> where T :IComparable
+    {
+        public T[] value;
+        public UC5_PrintMax(T[] array)
+        {
+            this.value = array;
+        }
+        public T[] Sort(T[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+        public T FindMaxValue()
+        {
+            T[] sorted = Sort(value);
+            return sorted[sorted.Length - 1];
+            
+        }
+    }
+}
