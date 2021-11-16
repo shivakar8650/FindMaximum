f79382a6ef5ebf076b14c91bafdf1df9245769e8
shivakar8650 <76126147+shivakar8650@users.noreply.github.com>
2021-11-16 20:12:01 +0530

    [ahivakar] update :all UC objects are called"

--- Program.cs
+++ Program.cs
@@ -8,14 +8,29 @@ namespace FindMaxUsingGeneric
         {
 
 
-            /*  UC1_Find_Max check = new UC1_Find_Max();
-              check.checkMax(3, 5, 5);*/
+            /*
+             UC1_Find_Max Check2 = new UC1_Find_Max();
+             Check2.checkMax(3, 5, 15);
+            */
 
-            /*  UC2_FindMaxFloat  check = new UC2_FindMaxFloat();
-              check.FindMax(1.5, 5.1565, 5.894);*/
-            UC3_FindMaxString check = new UC3_FindMaxString();
-            check.FindMax("44","260","56");
 
+            /* UC3_FindMaxString Check1 = new UC3_FindMaxString();
+             Check1.FindMax("apple", "peach", "banana");*/
+
+
+            /* UC4_Refactor_One_Method Check = new UC4_Refactor_One_Method();
+             Check.FindMax(78, 785, 78);
+             Check.FindMax(4.645, 45.6544, 48.984);
+             Check.FindMax<string>("apple", "peach", "banana");*/
+
+            //refector 2
+            /* GenricMaximum<double> check = new GenricMaximum<double>(4.645, 45.6544, 48.984);
+             Console.WriteLine(check.testMaximum());
+ */
+            // for UC5
+            int[] intArray = { 12, 5457, 45, 687, 6554, 6233, 487, 4622 };
+            UC5_PrintMax<int> check = new UC5_PrintMax<int>(intArray);
+            Console.WriteLine("Maximum is :" + check.FindMaxValue());
 
 
         }
